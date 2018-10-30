e6c7ff23807ee3f666b6424458c63c0d27c38cf8
Zamrony P. Juhara <zamronypj@yahoo.com>
2018-10-30 16:05:11 +0800

    initial implementation demo

---
+++ app/app.pas
@@ -0,0 +1,35 @@
+(*!------------------------------------------------------------
+ * Fano Web Framework Skeleton Application (https://fano.juhara.id)
+ *
+ * @link      https://github.com/zamronypj/fano-app
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/zamronypj/fano-app/blob/master/LICENSE (GPL 3.0)
+ *------------------------------------------------------------- *)
+program app;
+
+uses
+    fano,
+    myapp;
+
+    (*!-----------------------------------------------
+     * Bootstrap application
+     *
+     * @author Zamrony P. Juhara <zamronypj@yahoo.com>
+     *------------------------------------------------*)
+    procedure runApp();
+    var
+        appInstance : IWebApplication;
+        container : IDependencyContainer;
+    begin
+        container := TDependencyContainer.create(TDependencyList.create());
+        appInstance := TMyApp.create(
+            container,
+            TCGIEnvironment.create(),
+            TErrorHandler.create()
+        );
+        appInstance.run();
+    end;
+
+begin
+    runApp();
+end.
---
+++ app/myapp.pas
@@ -0,0 +1,56 @@
+(*!------------------------------------------------------------
+ * Fano Web Framework Skeleton Application (https://fano.juhara.id)
+ *
+ * @link      https://github.com/zamronypj/fano-app
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/zamronypj/fano-app/blob/master/LICENSE (GPL 3.0)
+ *------------------------------------------------------------- *)
+unit myapp;
+
+interface
+
+uses
+    fano;
+
+type
+
+    TMyApp = class(TFanoWebApplication)
+    protected
+        procedure buildDependencies(const container : IDependencyContainer); override;
+        procedure buildRoutes(const container : IDependencyContainer); override;
+        function initDispatcher(const container : IDependencyContainer) : IDispatcher; override;
+    end;
+
+implementation
+
+uses
+
+    sysutils,
+
+    {*! -------------------------------
+        controllers factory
+    ----------------------------------- *}
+    HiControllerFactory;
+
+
+    procedure TMyApp.buildDependencies(const container : IDependencyContainer);
+    begin
+        {$INCLUDE Dependencies/dependencies.inc}
+    end;
+
+    procedure TMyApp.buildRoutes(const container : IDependencyContainer);
+    var router : IRouteCollection;
+    begin
+        router := container.get('router') as IRouteCollection;
+        try
+            {$INCLUDE Routes/routes.inc}
+        finally
+            router := nil;
+        end;
+    end;
+
+    function TMyApp.initDispatcher(const container : IDependencyContainer) : IDispatcher;
+    begin
+        result := container.get('dispatcher') as IDispatcher;
+    end;
+end.
