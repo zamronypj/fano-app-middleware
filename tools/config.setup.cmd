152e173dc02edc501937a1ef13857a1516e1bb14
Zamrony P. Juhara <zamronypj@yahoo.com>
2018-11-06 17:57:03 +0800

    edit url in comment

--- tools/config.setup.cmd
+++ tools/config.setup.cmd
@@ -1,9 +1,9 @@
 REM------------------------------------------------------------
 REM Fano Web Framework Skeleton Application (https://fanoframework.github.io)
 REM
-REM @link      https://github.com/fanoframework/fano-app
+REM @link      https://github.com/fanoframework/fano-app-middleware
 REM @copyright Copyright (c) 2018 Zamrony P. Juhara
-REM @license   https://github.com/fanoframework/fano-app/blob/master/LICENSE (GPL 3.0)
+REM @license   https://github.com/fanoframework/fano-app-middleware/blob/master/LICENSE (GPL 3.0)
 REM-------------------------------------------------------------
 
 REM------------------------------------------------------
